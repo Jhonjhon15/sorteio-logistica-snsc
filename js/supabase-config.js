fda3450d43c120f7d370c7976a037763b79638f6
Jhonjhon15 <mendesjhonatan78@gmail.com>
2026-09-12 14:58:45 -0300

    Fix: Separar index.html (capa) e instituicoes.html (listagem)

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,10 +1,6 @@
 const SUPABASE_URL = 'https://vjciwtsyffejghxlpygb.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqY2l3dHN5ZmZlamdoeGxweWdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNjA1NTksImV4cCI6MjEwNDczNjU1OX0.cCwYBTVbKq0So_lNz-t-rbSujsD2c2D-8qvgiORCG2o';
-
-if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-  console.error('ERRO: URL ou Chave do Supabase não configuradas!');
-} else {
-  console.log('Credenciais do Supabase carregadas com sucesso.');
-}
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqY2l3dHN5ZmZlbGdodHhscHlnYiIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzg5MTYwNTU5LCJleHAiOjIxMDQ3MzY1NTl9.cCwYBTVbKq0So_lNz-t-rbSujsD2c2D-8qvgiORCG2o';
 
 window._supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+console.log(' Supabase inicializado com sucesso');
